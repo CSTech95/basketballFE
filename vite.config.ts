1d6d28362a95527f85e75bc78ea36022359da687
Aser <adocteur@gmail.com>
2023-10-27 00:45:56 -0400

    Edit: Github Actions Impl

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
   },
-  base: "/",
+  base: "/CSTech95/basketballFE/",
   test: {
     globals: true,
     environment: "jsdom",
