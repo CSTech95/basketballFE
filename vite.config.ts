ac94f17abcf9a66d9cac819bd959996e8a470728
Aser <adocteur@gmail.com>
2023-10-27 12:09:19 -0400

    Edit: Github Actions Impl

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //  server: {
-  //    open: true,
-  //  },
+  server: {
+    open: true,
+    https: true,
+  },
   build: {
     outDir: "dist",
     sourcemap: true,
