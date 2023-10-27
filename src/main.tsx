ebe5dd9706b06c14b0c85316fa8e3bfa2345e0ff
Aser <adocteur@gmail.com>
2023-10-27 15:12:26 -0400

    Edit: Github Actions Impl

--- src/main.tsx
+++ src/main.tsx
@@ -8,32 +8,12 @@ import Home from "./features/home/Home"
 import QueryPlayers from "./features/queryPlayers/QueryPlayers"
 import { QueryGames } from "./features/querygames/QueryGames"
 import { QueryClient, QueryClientProvider } from "react-query"
-import { Navbar } from "./features/navbar/Navbar"
-import { StatSlideshow } from "./features/statSlideshow/StatSlideshow"
-import Cover from "./features/cover/Cover"
-import MyComponent from "./features/samples/MyComponent"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     children: [
-      {
-        path: "/",
-        element: (
-          <div className="flex flex-col">
-            <StatSlideshow />
-            <Cover />
-            <div className="grid grid-cols-1 grid-gap-4 border border-black md:grid-cols-2">
-              <QueryPlayers />
-              <QueryGames />
-            </div>
-            <div className="h-[1196px] bg-stone-950 border">
-              <MyComponent />
-            </div>
-          </div>
-        ),
-      },
       {
         path: "about",
         element: <h1>About</h1>,
