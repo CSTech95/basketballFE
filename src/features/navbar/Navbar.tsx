9d42d748d29a7d59b78392b66f6aa3808fe8a4b0
Aser <adocteur@gmail.com>
2023-10-27 16:17:22 -0400

    Edit: Github Actions Impl

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ export function Navbar() {
   return (
     <nav className="rounded-b-2xl flex flex-row justify-between h-[55px] px-4  bg-zinc-950  w-full md:w-auto">
       <div className="flex align-center items-center">
-        <NavLink to="/basketballFE">
+        <NavLink to="/basketballFE/">
           <img
             src="https://cdn.nba.com/logos/leagues/logo-nba.svg"
             className="flex h-[30px]"
