bd037d73feaf7195bff466a7f840ff3d3da0c066
Aser <adocteur@gmail.com>
2023-10-31 00:06:38 -0400

    Edit: Table takes in data

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import counterReducer from "../features/counter/counterSlice"
-import { ballDontLieApi } from "../features/counter/nba-query"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    [ballDontLieApi.reducerPath]: ballDontLieApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(ballDontLieApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
 })
 
 export type AppDispatch = typeof store.dispatch
