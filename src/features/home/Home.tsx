e5ed4dbc7cfe23135f84ba1bea7b296dc9516b47
Aser <adocteur@gmail.com>
2023-11-03 22:02:22 -0400

    Edit: Navbar responsiveness

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,27 +1,11 @@
-import React from "react"
-import Cover from "../cover/Cover"
 import { Footer } from "../footer/Footer"
-//import { Navbar_ } from "../navbar/Navbar_"
-import QueryPlayers from "../queryPlayers/QueryPlayers"
-import { QueryGames } from "../querygames/QueryGames"
-import MyComponent from "../samples/MyComponent"
-import { StatSlideshow } from "../statSlideshow/StatSlideshow"
 import { Outlet } from "react-router-dom"
-import Navbar_ from "../navbar/Navbar_"
+import Navbar from "../navbar/Navbar"
 
 const Home = () => {
   return (
-    <div className="flex flex-col ">
-      <Navbar_ />
-      {/*<StatSlideshow />
-      <Cover />
-      <div className="grid grid-cols-1 grid-gap-4 border border-black md:grid-cols-2">
-        <QueryPlayers />
-        <QueryGames />
-      </div>
-      <div className="h-[1196px] bg-stone-950">
-        <MyComponent />
-      </div>*/}
+    <div className="box-border m-0 p-0 ">
+      <Navbar />
       <div className="mb-auto">
         <Outlet />
       </div>
--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,23 +1,55 @@
-import { Link, NavLink } from "react-router-dom"
+import React from "react"
+import { NavLink } from "react-router-dom"
+import burgerImg from "../../images/burger.svg"
 
-export function Navbar() {
+export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false)
   return (
-    <nav className="rounded-b-2xl flex flex-row justify-between h-[55px] px-4  bg-zinc-950  w-full md:w-auto">
-      <div className="flex align-center items-center">
-        <NavLink to="/basketballFE/">
-          <img
-            src="https://cdn.nba.com/logos/leagues/logo-nba.svg"
-            className="flex h-[30px]"
-          />
-        </NavLink>
+    <nav className="relative flex flex-col flex-wrap items-center justify-between px-4 py-3 bg-slate-900  rounded-b-2xl mb-3 sm:flex-col">
+      <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+        <div className="text-sm font-bold leading-relaxed inline-block mr-2 py-2 whitespace-nowrap uppercase text-white">
+          <NavLink to="/basketballFE/">
+            <img
+              src="https://cdn.nba.com/logos/leagues/logo-nba.svg"
+              className="flex h-[30px]"
+            />
+          </NavLink>
+        </div>
+        <button
+          className="text-white bg-white border-white cursor-pointer text-xl leading-none px-2 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button"
+          onClick={() => setNavbarOpen(!navbarOpen)}
+        >
+          <img className="h-8" src={burgerImg} />
+          <i className="fas fa-bars"></i>
+        </button>
       </div>
-
-      <div className=" text-white items-center flex order-last space-x-7">
-        {/*<button className="order-last">Sign In</button>*/}
-        {/*<NavLink to="/teams">Teams</NavLink>*/}
-        <NavLink to="/basketballFE/players">Players</NavLink>
-        <NavLink to="/basketballFE/games">Games</NavLink>
-        {/*<NavLink to="/stats">Stats</NavLink>*/}
+      <div
+        className={
+          "lg:flex  items-center " + (navbarOpen ? " flex" : " hidden")
+        }
+        id="example-navbar-danger"
+      >
+        <ul className="flex flex-col lg:flex-row list-none text-lg ">
+          <li className="nav-item">
+            <div className="px-3 py-2 flex items-center  uppercase font-bold leading-snug text-white hover:opacity-75">
+              <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75" />
+              <NavLink to="/basketballFE/players">Players</NavLink>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75">
+              <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
+              <NavLink to="/basketballFE/games">Games</NavLink>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75">
+              <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+              <NavLink to="/basketballFE/about">About</NavLink>
+            </div>
+          </li>
+        </ul>
       </div>
     </nav>
   )
